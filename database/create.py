ecbb1c3b29c03de4872f21a76cdc718543813a20
ivan.v.eliseev <ivan.v.eliseev@south.rt.ru>
2023-11-01 19:14:52 +0300

    add create.py

---
+++ database/create.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+from database.session import session
+from database.query import Query
+
+create_user_table = '''
+CREATE TABLE IF NOT EXISTS student (
+		id INTEGER PRIMARY KEY,
+		name VARCHAR,
+		tgusername VARCHAR,
+		chatid VARCHAR,
+		birthday VARCHAR,
+		isadmin INTEGER
+	);
+'''
+
+
+def rollout():
+    with session() as s:
+        s.execute(Query(create_user_table))
