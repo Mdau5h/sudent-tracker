75d62620c71ee8b06bd4679d50e909efae56c9a3
mdau5h <adeswepore73@gmail.com>
2023-12-26 22:15:44 +0300

    add basic aiogram

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
-from database.create import rollout
+import asyncio
+import logging
+import sys
 
-rollout()
+from aiogram import Bot
+from aiogram.enums import ParseMode
+from app.handlers import dp
+from app.config import config
+
+
+async def main() -> None:
+    bot = Bot(config.TOKEN, parse_mode=ParseMode.HTML)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO, stream=sys.stdout)
+    asyncio.run(main())
