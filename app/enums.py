cbed4dc869e287089b50785963c8e5340624e8ed
mdau5h <adeswepore73@gmail.com>
2024-01-02 17:33:13 +0300

    refactor static messages

--- app/enums.py
+++ app/enums.py
@@ -3,10 +3,11 @@ from enum import Enum
 
 class StaticMessages(str, Enum):
     HELLO_MESSAGE = "Here you are again!\n"
-    HELLO_MESSAGE_UNKNOWN = ("Hi, i'm Student Tracker 🤗\n"
+    HELLO_UNKNOWN_MESSAGE = ("Hi, i'm Student Tracker 🤗\n"
                              "Seems like we don't know each other.\n"
                              "Got access code? Type /code to pass it, so I could remember you!\n")
     ENTER_CODE_MESSAGE = "Enter access code:\n"
-    INCORRECT_CODE = "Incorrect code! Please, try again! 🙂\n"
-    OUT_OF_ATTEMPTS = "Oops! 😬\nYou have reached the limit of attempts\n"
-    ACCESS_GRANTED = "Hello, mister admin 😎 Type /help to see all available commands\n"
+    INCORRECT_CODE_MESSAGE = "Incorrect code! Please, try again! 🙂\n"
+    OUT_OF_ATTEMPTS_MESSAGE = "Oops! 😬\nYou have reached the limit of attempts\n"
+    ACCESS_GRANTED_MESSAGE = "Hello, mister admin 😎 Type /help to see all available commands\n"
+    HELP_MESSAGE = "You shouldn't see this if you are not authorised\n"
