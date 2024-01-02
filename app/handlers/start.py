53f235cad5b10b499f3419da8b46b1c7f8c5438b
mdau5h <adeswepore73@gmail.com>
2024-01-02 11:33:04 +0300

    add fsm handlers for auth

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -1,28 +1,48 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.filters import Command, CommandStart
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
 from database.ext.users import (
     get_user_by_id,
     save_user
 )
 from app.enums import StaticMessages
+from app.states import UserStates
+from app.config import config
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def command_start_handler(message: Message) -> None:
-    if not get_user_by_id(message.from_user.id):
+async def command_start_handler(message: Message, state: FSMContext) -> None:
+    user_authorised = get_user_by_id(message.from_user.id)
+    if not user_authorised:
+        await state.set_state(UserStates.new_user)
         await message.answer(StaticMessages.HELLO_MESSAGE_UNKNOWN)
     else:
-        await message.answer(StaticMessages.HELLO_MESSAGE)
+        msg = StaticMessages.HELLO_MESSAGE
+        if user_authorised.is_admin:
+            msg += StaticMessages.ACCESS_GRANTED
+        await message.answer(msg)
 
 
-@router.message(Command('code'))
-async def enter_code_handler(message: Message) -> None:
+@router.message(UserStates.new_user, Command('code'))
+async def enter_code_handler(message: Message, state: FSMContext) -> None:
+    await state.set_state(UserStates.code)
+    await message.answer(StaticMessages.ENTER_CODE_MESSAGE)
+
+
+@router.message(UserStates.code, F.text.casefold() == config.ACCESS_CODE)
+async def process_code(message: Message, state: FSMContext):
     user = {
         'id': message.from_user.id,
         'is_admin': True
     }
     save_user(**user)
-    await message.answer(StaticMessages.ENTER_CODE_MESSAGE)
+    await state.clear()
+    await message.answer(StaticMessages.ACCESS_GRANTED)
+
+
+@router.message(UserStates.code)
+async def process_code_incorrect(message: Message):
+    await message.answer(StaticMessages.INCORRECT_CODE)
