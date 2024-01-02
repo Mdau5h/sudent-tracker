bebf9409e31102b614c982709c9b76ff3dbf3eaa
mdau5h <adeswepore73@gmail.com>
2024-01-02 14:13:48 +0300

    add attempts handler

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -9,6 +9,7 @@ from database.ext.users import (
 from app.enums import StaticMessages
 from app.states import UserStates
 from app.config import config
+from server import logger
 
 router = Router()
 
@@ -29,6 +30,7 @@ async def command_start_handler(message: Message, state: FSMContext) -> None:
 @router.message(UserStates.new_user, Command('code'))
 async def enter_code_handler(message: Message, state: FSMContext) -> None:
     await state.set_state(UserStates.code)
+    await state.update_data(attempts=config.CODE_ATTEMPTS)
     await message.answer(StaticMessages.ENTER_CODE_MESSAGE)
 
 
@@ -44,5 +46,11 @@ async def process_code(message: Message, state: FSMContext):
 
 
 @router.message(UserStates.code)
-async def process_code_incorrect(message: Message):
-    await message.answer(StaticMessages.INCORRECT_CODE)
+async def process_code_incorrect(message: Message, state: FSMContext):
+    state_data = await state.get_data()
+    await state.update_data(attempts=state_data['attempts'] - 1)
+    if await state.get_data() != {'attempts': 0}:
+        await message.answer(StaticMessages.INCORRECT_CODE)
+    else:
+        await message.answer(StaticMessages.OUT_OF_ATTEMPTS)
+        await state.clear()
