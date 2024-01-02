1e82d85a443b7a30f418d025bcc1424e72994e42
mdau5h <adeswepore73@gmail.com>
2024-01-02 14:13:33 +0300

    add number of auth attempts

--- app/config.py
+++ app/config.py
@@ -8,6 +8,7 @@ class Config:
     TOKEN: str = os.getenv('TOKEN')
     ACCESS_CODE: str = os.getenv('ACCESS_CODE')
     APP_NAME: str = 'student-tracker'
+    CODE_ATTEMPTS: int = 5
 
 
 config = Config()
