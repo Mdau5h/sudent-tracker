b72f4a4c89038c1af31ec25ed94f0ee63c30218f
mdau5h <adeswepore73@gmail.com>
2024-01-02 11:33:25 +0300

    configure static messages

--- app/enums.py
+++ app/enums.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 
 class StaticMessages(str, Enum):
-    HELLO_MESSAGE = "Hello again!"
-    HELLO_MESSAGE_UNKNOWN = "Hi, i'm Student Tracker 🤗\nSeems like we don't know each other.\nGot access code? Type /code to pass it, so I could remember you!"
-    ENTER_CODE_MESSAGE = "Enter access code:"
+    HELLO_MESSAGE = "Here you are again!\n"
+    HELLO_MESSAGE_UNKNOWN = "Hi, i'm Student Tracker 🤗\nSeems like we don't know each other.\nGot access code? Type /code to pass it, so I could remember you!\n"
+    ENTER_CODE_MESSAGE = "Enter access code:\n"
+    INCORRECT_CODE = "Incorrect code 🙂\n"
+    ACCESS_GRANTED = "Hello, mister admin 😎 Type /help to see all available commands\n"
