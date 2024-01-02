d030619f19fd7130f67e8ca34b2882701d50938b
mdau5h <adeswepore73@gmail.com>
2024-01-02 17:35:01 +0300

    refactor static messades and routers

--- app/handlers/start.py
+++ app/handlers/start.py
@@ -10,30 +10,30 @@ from app.enums import StaticMessages
 from app.states import UserStates
 from app.config import config
 
-router = Router()
+start_router = Router()
 
 
-@router.message(CommandStart())
+@start_router.message(CommandStart())
 async def command_start_handler(message: Message, state: FSMContext) -> None:
     user_authorised = get_user_by_id(message.from_user.id)
     if not user_authorised:
         await state.set_state(UserStates.new_user)
-        await message.answer(StaticMessages.HELLO_MESSAGE_UNKNOWN)
+        await message.answer(StaticMessages.HELLO_UNKNOWN_MESSAGE)
     else:
         msg = StaticMessages.HELLO_MESSAGE
         if user_authorised.is_admin:
-            msg += StaticMessages.ACCESS_GRANTED
+            msg += StaticMessages.ACCESS_GRANTED_MESSAGE
         await message.answer(msg)
 
 
-@router.message(UserStates.new_user, Command('code'))
+@start_router.message(UserStates.new_user, Command('code'))
 async def enter_code_handler(message: Message, state: FSMContext) -> None:
     await state.set_state(UserStates.code)
     await state.update_data(attempts=config.CODE_ATTEMPTS)
     await message.answer(StaticMessages.ENTER_CODE_MESSAGE)
 
 
-@router.message(UserStates.code, F.text.casefold() == config.ACCESS_CODE)
+@start_router.message(UserStates.code, F.text.casefold() == config.ACCESS_CODE)
 async def process_code(message: Message, state: FSMContext):
     user = {
         'id': message.from_user.id,
@@ -41,15 +41,15 @@ async def process_code(message: Message, state: FSMContext):
     }
     save_user(**user)
     await state.clear()
-    await message.answer(StaticMessages.ACCESS_GRANTED)
+    await message.answer(StaticMessages.ACCESS_GRANTED_MESSAGE)
 
 
-@router.message(UserStates.code)
+@start_router.message(UserStates.code)
 async def process_code_incorrect(message: Message, state: FSMContext):
     state_data = await state.get_data()
     await state.update_data(attempts=state_data['attempts'] - 1)
     if await state.get_data() != {'attempts': 0}:
-        await message.answer(StaticMessages.INCORRECT_CODE)
+        await message.answer(StaticMessages.INCORRECT_CODE_MESSAGE)
     else:
-        await message.answer(StaticMessages.OUT_OF_ATTEMPTS)
+        await message.answer(StaticMessages.OUT_OF_ATTEMPTS_MESSAGE)
         await state.clear()
