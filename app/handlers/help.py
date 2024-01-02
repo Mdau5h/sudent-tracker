cb0c370392558bdaec21dffd28859de5552474da
mdau5h <adeswepore73@gmail.com>
2024-01-02 17:33:44 +0300

    add mock help handler

---
+++ app/handlers/help.py
@@ -0,0 +1,14 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+from app.enums import StaticMessages
+from app.auth import auth
+
+
+help_router = Router()
+
+
+@help_router.message(Command('help'))
+@auth
+async def help_handler(message: Message) -> None:
+    await message.answer(StaticMessages.HELP_MESSAGE)
