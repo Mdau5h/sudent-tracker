5240d0c36f91a9ff6e77d8d6e497a85471871bbf
mdau5h <adeswepore73@gmail.com>
2024-01-03 16:30:10 +0300

    add get_student_by_id and fix update

--- database/raw/students.py
+++ database/raw/students.py
@@ -4,8 +4,9 @@ from database.query import Query
 def create_student_query(
         teacher_id: int,
         student_name: str,
-        paid_lessons: int,
-        given_lessons: int,
+        paid_lessons: int = 0,
+        given_lessons: int = 0,
+        lesson_diff: int = 0,
         is_active: bool = True
 ) -> Query:
     bound_params = {
@@ -13,7 +14,7 @@ def create_student_query(
         'student_name': student_name,
         'paid_lessons': paid_lessons,
         'given_lessons': given_lessons,
-        'lesson_diff': paid_lessons - given_lessons,
+        'lesson_diff': lesson_diff,
         'is_active': is_active
     }
 
@@ -44,32 +45,44 @@ def create_student_query(
 
 def update_student_query(
         id: int,
-        teacher_id: int,
-        student_name: str,
-        paid_lessons: int,
-        given_lessons: int,
-        is_active: bool = True
+        **fields,
 ) -> Query:
     bound_params = {
         'id': id,
-        'teacher_id': teacher_id,
-        'student_name': student_name,
-        'paid_lessons': paid_lessons,
-        'given_lessons': given_lessons,
-        'lesson_diff': paid_lessons - given_lessons,
-        'is_active': is_active
+        **fields,
+    }
+
+    fields_template = ",".join((
+        f"{field_name}=:{field_name}"
+        for field_name in fields.keys()
+    ))
+
+    query = Query(
+        f'''
+        UPDATE student SET {fields_template} WHERE id=:id;
+        ''',
+        bound_params,
+    )
+
+    return query
+
+
+def get_student_by_id_query(id_: int) -> Query:
+    bound_params = {
+        'id': id_
     }
 
     query = Query(
         '''
-        UPDATE student SET
-            teacher_id=:teacher_id,
-            student_name=:student_name,
-            paid_lessons=:paid_lessons,
-            given_lessons=:given_lessons,
-            lesson_diff=:lesson_diff,
-            is_active=:is_active
-        WHERE id=:id;
+        SELECT 
+            id, 
+            teacher_id,
+            student_name,
+            paid_lessons,
+            given_lessons,
+            lesson_diff,
+            is_active
+        FROM student WHERE id=:id;
         ''',
         bound_params,
     )
