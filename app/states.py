276d913c1e1f22ef83711d393417880c84b8b7b8
mdau5h <adeswepore73@gmail.com>
2024-01-03 19:40:45 +0300

    add states for create student

--- app/states.py
+++ app/states.py
@@ -4,3 +4,9 @@ from aiogram.fsm.state import State, StatesGroup
 class UserStates(StatesGroup):
     new_user = State()
     code = State()
+
+
+class CreateStudentStates(StatesGroup):
+    name = State()
+    paid_lessons = State()
+    given_lessons = State()
