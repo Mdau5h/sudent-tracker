fade699a2e808177bad190875d2539f926b6c1ec
mdau5h <adeswepore73@gmail.com>
2024-01-03 11:29:18 +0300

    add model student

--- database/models.py
+++ database/models.py
@@ -5,3 +5,13 @@ from dataclasses import dataclass
 class User:
     id: int
     is_admin: bool
+
+
+@dataclass
+class Student:
+    id: int
+    teacher_id: int
+    student_name: str
+    paid_lessons: int
+    given_lessons: int
+    is_active: bool
