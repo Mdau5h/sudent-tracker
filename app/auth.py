abcae26441cbd02ef4a80d4844f08eb0c8f5eeec
mdau5h <adeswepore73@gmail.com>
2024-01-05 22:17:49 +0300

    add states handle to wrap func got rid of circular imports

--- app/auth.py
+++ app/auth.py
@@ -1,12 +1,12 @@
 from functools import wraps
 from aiogram.types import Message
 from database.ext.users import get_user_by_id
-from server import logger
 
 
 def auth(command_handler):
     @wraps(command_handler)
-    async def handle(message: Message):
+    async def handle(message: Message, **kwargs):
+        from server import logger
         user_id = get_user_by_id(message.from_user.id)
         if not user_id:
             logger.info(f"Message from unauthorised user! ID: {message.from_user.id}")
@@ -14,5 +14,5 @@ def auth(command_handler):
         if not user_id.is_admin:
             logger.info(f"Message from non-admin user! ID: {message.from_user.id}")
             return
-        return await command_handler(message)
+        return await command_handler(message, **kwargs)
     return handle
