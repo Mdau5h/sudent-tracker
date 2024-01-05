2ad38a9e75495eaba22d8b06e41416cda1377583
mdau5h <adeswepore73@gmail.com>
2024-01-05 22:19:51 +0300

    add students handlers and routers

---
+++ app/handlers/students.py
@@ -0,0 +1,56 @@
+from aiogram import Router
+from aiogram.filters import Command
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from app.states import CreateStudentStates
+from app.enums import CreateStudentForm
+from database.ext.students import save_student
+from app.auth import auth
+
+students_router = Router()
+
+
+@students_router.message(Command('create'))
+@auth
+async def create_student_handler(message: Message, state: FSMContext) -> None:
+    await state.set_state(CreateStudentStates.name)
+    await message.answer(CreateStudentForm.ENTER_NAME_MESSAGE)
+
+
+@students_router.message(CreateStudentStates.name)
+@auth
+async def enter_student_name(message: Message, state: FSMContext) -> None:
+    from server import logger
+
+    await state.update_data(student_name=message.text)
+    data = await state.get_data()
+    logger.info(f'Stored data: {data}')
+    await state.set_state(CreateStudentStates.paid_lessons)
+    await message.answer(CreateStudentForm.ENTER_PAID_MESSAGE)
+
+
+@students_router.message(CreateStudentStates.paid_lessons)
+@auth
+async def enter_student_name(message: Message, state: FSMContext) -> None:
+    from server import logger
+
+    await state.update_data(paid_lessons=int(message.text))
+    data = await state.get_data()
+    logger.info(f'Stored data: {data}')
+    await state.set_state(CreateStudentStates.given_lessons)
+    await message.answer(CreateStudentForm.ENTER_GIVEN_MESSAGE)
+
+
+@students_router.message(CreateStudentStates.given_lessons)
+@auth
+async def enter_paid_lessons(message: Message, state: FSMContext) -> None:
+    from server import logger
+
+    await state.update_data(given_lessons=int(message.text))
+    data = await state.get_data()
+    logger.info(f'Stored data: {data}')
+    data['teacher_id'] = message.from_user.id
+    data['lesson_diff'] = data['paid_lessons'] - data['given_lessons']
+    save_student(**data)
+    await state.clear()
+    await message.answer(CreateStudentForm.ENTER_COMPLETE_MESSAGE)
