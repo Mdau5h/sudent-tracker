db728979edd4971085afa5d915bb31f98f716ceb
mdau5h <adeswepore73@gmail.com>
2024-01-09 18:10:54 +0300

    fix student response

--- app/utils.py
+++ app/utils.py
@@ -15,8 +15,9 @@ def format_student_info(student: Student):
            f"Paid lessons: {student.paid_lessons}\n"
            f"Given lessons: {student.given_lessons}\n"
            f"Lessons left: {student.lesson_diff}\n"
-           f"Status: {'Active' if student.is_active else 'Inactive'}\n\n"
-           + CommandsList.FOR_STUDENTS +
-           f"\n/all - go back"
+           f"Status: {'Active' if student.is_active else 'Inactive'}\n"
+           f"{'Comment: ' + student.comment + chr(10) if student.comment else ''}"
+           + "\n" + CommandsList.FOR_STUDENTS +
+           f"\n\n/all - go back"
            )
     return msg
