265218acd40e1471dd927f00de126547d3d4b2ab
mdau5h <adeswepore73@gmail.com>
2024-01-09 17:47:47 +0300

    add command list for students

--- app/commands.py
+++ app/commands.py
@@ -1,6 +1,14 @@
-def for_admin() -> list[str]:
+def for_admin_user() -> list[str]:
     # todo: make it dynamic and smarter
     return [
         '/create - add new student',
         '/all - see all your students',
+    ]
+
+
+def for_students() -> list[str]:
+    return [
+        "/spend - spend lesson",
+        "/add - add paid lessons",
+        "/comment - add or change comment"
     ]
