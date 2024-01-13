c75e2507f6ac6f2250d13f07160336fea8b81949
mdau5h <adeswepore73@gmail.com>
2024-01-13 20:33:15 +0300

    add markup to help handler

--- app/handlers/help.py
+++ app/handlers/help.py
@@ -2,6 +2,7 @@ from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
 from app.enums import CommandsList
+from app.keyboards import init_markup
 from app.auth import auth
 
 
@@ -11,4 +12,4 @@ help_router = Router()
 @help_router.message(Command('help'))
 @auth
 async def help_handler(message: Message) -> None:
-    await message.answer(CommandsList.FOR_ADMIN_USER)
+    await message.answer(CommandsList.FOR_ADMIN_USER, reply_markup=init_markup)
