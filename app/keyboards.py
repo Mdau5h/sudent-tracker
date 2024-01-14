29331eceebcfe7db57bd5315de8d1b9031caa789
mdau5h <adeswepore73@gmail.com>
2024-01-14 21:12:17 +0300

    add student info keyboard

--- app/keyboards.py
+++ app/keyboards.py
@@ -9,8 +9,25 @@ from aiogram.types import (
 init_markup = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text='✍️ Add new student'),
+            KeyboardButton(text='👨‍🎓 Add new student'),
             KeyboardButton(text='📋 See list of your students')
+        ]
+    ],
+    resize_keyboard=True
+)
+
+student_info_markup = ReplyKeyboardMarkup(
+    keyboard=[
+        [
+            KeyboardButton(text='✅ Spend lesson'),
+            KeyboardButton(text='➕ Add paid lessons')
+        ],
+        [
+            KeyboardButton(text='✍️ Add or change comment'),
+            KeyboardButton(text='🗑️ Delete student')
+        ],
+        [
+            KeyboardButton(text='🔙 Go back')
         ]
     ],
     resize_keyboard=True
