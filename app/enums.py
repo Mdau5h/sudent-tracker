8053614363fae7582414a1c853bcd8ab9f9a0eb9
mdau5h <adeswepore73@gmail.com>
2024-01-15 23:38:47 +0300

    reformat enums.py

--- app/enums.py
+++ app/enums.py
@@ -33,27 +33,19 @@ class StudentForm(str, Enum):
     ENTER_PAID_MESSAGE = "Enter number of paid lessons:\n"
     ENTER_GIVEN_MESSAGE = "Enter number of given lessons:\n"
     ENTER_COMPLETE_MESSAGE = ("Cool! I've saved the student's info! 👌\n"
-                              "You can see them in the list")
+                              "You can see them in the list by hitting:\n"
+                              f'"{ButtonList.STUDENT_LIST_BUTTON.value}"\n')
     INCORRECT_INPUT_MESSAGE = "Sorry, I didn't get you. Throw me the number, please 🙂\n"
     ENTER_COMMENT_MESSAGE = "Enter the comment: \n"
     CONFIRM_MESSAGE = "Are you sure? There's no undo! 🤔\n"
-    LIST_MESSAGE = ("Here's the list of all your students.\n"
-                    "Click on their ID on the left to see more info 😉\n")
+    LIST_MESSAGE = ("Here's the list of all your students 👇\n"
+                    "Click on them to see more info 😉\n")
+    ADDITIONAL_MESSAGE = ("Or you can add a new one! \n"
+                          f'Hit the button: "{ButtonList.CREATE_STUDENT_BUTTON.value}" to do so!\n')
     EMPTY_LIST_MESSAGE = ("Seems like you don't any students yet. 🤔\n"
-                          "Not a big deal! Let's create one!\n")
-    STUDENT_UPDATED_MESSAGE = "Great! Here's the updated student info:\n"
+                          "Not a big deal! Let's create one!\n"
+                          f'Hit the button: "{ButtonList.CREATE_STUDENT_BUTTON.value}" \n'
+                          )
+    STUDENT_UPDATED_MESSAGE = "Great! 🤗 Here's the updated student info:\n"
     STUDENT_DELETED_MESSAGE = "Done! Student has been deleted! 👌\n"
     DELETE_CANCELED_MESSAGE = "Deletion hasn't been confirmed 🤷\n"
-
-
-class ButtonList(str, Enum):
-    ENTER_CODE_BUTTON = '🔑 Enter code'
-    CREATE_STUDENT_BUTTON = '👨‍🎓 Add new student'
-    STUDENT_LIST_BUTTON = '📋 See list of your students'
-    SPEND_LESSON_BUTTON = '✅ Spend lesson'
-    ADD_LESSONS_BUTTON = '➕ Add paid lessons'
-    COMMENT_BUTTON = '✍️ Add or change comment'
-    DELETE_STUDENT_BUTTON = '🗑️ Delete student'
-    GO_BACK_BUTTON = '🔙 Go back'
-    YES_BUTTON = '✅ Yes'
-    NO_BUTTON = '🚫 No'
